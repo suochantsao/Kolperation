7989c06cc91dd7de3d1311bb859ad7d353926ffe
suochantsao <suochantsao@gmail.com>
2021-09-02 17:54:10 +0800

    [debug] Reset Trial doc name

--- src/views/trial.vue
+++ src/views/trial.vue
@@ -1,29 +1,27 @@
 <template>
-    <div id="app">
-        <home-header></home-header>
+  <div id="app">
+    <home-header></home-header>
 
-        <div class="container trialContent">
-            <h2><span>合作案件搜尋</span></h2>  
+    <div class="container trialContent">
+      <h2><span>合作案件搜尋</span></h2>
 
-            <router-view />
-
-        </div>
-
-        <home-footer></home-footer>
+      <router-view />
     </div>
+
+    <home-footer></home-footer>
+  </div>
 </template>
 
 <script>
 // Components
-import homeHeader  from '../components/home-header.vue'
-import homeFooter  from '../components/home-footer.vue'
+import homeHeader from "../components/home-header.vue";
+import homeFooter from "../components/home-footer.vue";
 
 export default {
-    name: 'trial',
-    components: {
-        homeHeader,
-        homeFooter,
-    }
-}
-
+  name: "trial",
+  components: {
+    homeHeader,
+    homeFooter,
+  },
+};
 </script>
